b92a7c9d749877e2f385aca438fbc25cec0ef818
iamdineshgautam <deploywithdinesh@gmail.com>
2026-07-22 16:26:39 +0530

    day-1

--- day-1/resource.tf
+++ day-1/resource.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2" {
     ami = "ami-01a00762f46d584a1"
     instance_type = "t3.micro"
-    key_name = "bastion-sg"
+    key_name = "u3-key"
     tags = {
       Name = "server"
     }
