67298c0af344b3a8bf3f716c28d8e9a0294047e0
iamdineshgautam <deploywithdinesh@gmail.com>
2026-08-03 16:19:36 +0530

    day-9-lifecycle-blocks

--- day-8-state-management/provider.tf
+++ day-8-state-management/provider.tf
@@ -1,3 +1,7 @@
+# Terraform state management is the practice of storing, securing, and maintaining Terraform's state file (terraform.tfstate), 
+# which maps the resources in your configuration to the actual infrastructure that exists in your cloud or datacenter. 
+# Terraform uses this state to determine what needs to be created, updated, or destroyed during each plan and apply.
+
 provider "aws" {
   region = "ap-south-1"
   profile = "dev"
@@ -7,7 +11,7 @@ terraform {
   backend "s3" {
     bucket = "deploywithdinesh.space"
     region = "ap-south-1"
-    key = "terraform.tfstate"
+    key = "terraform.tfstate"   #The path/name of the state file inside S3.
     profile = "dev"
     use_lockfile = true
     shared_config_files = [ "/root/.aws/credentials" ]
---
+++ day-9-lifecycle-blocks/PD.tf
@@ -0,0 +1,16 @@
+# provider "aws" {
+#   region = "ap-south-1"
+#   profile = "dev"
+# }
+
+# resource "aws_s3_bucket" "s3" {
+#   bucket = "deploywithdinesh.space"
+
+#   tags = {
+#     Name = "dinesh.space"
+#   }
+
+#   lifecycle {
+#     prevent_destroy = true
+#   }
+# }
